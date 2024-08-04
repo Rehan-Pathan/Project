6d8861e5ace34b764d1d74e3354beab920df34ab
Rehan-Pathan <rehanpathanofficial@gmail.com>
2024-08-04 18:27:05 +0530

    Phase 3

--- API/Program.cs
+++ API/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseCors(opt=>{
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001");
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
 });
 app.UseHttpsRedirection();
 
